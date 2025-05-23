0f06e1af36cb331321a801497ba930f20390a1c7
Faza Affandi <affandifaza@gmail.com>
2025-05-23 17:00:23 +0700

    feat: add network helper for checking network condition

---
+++ MovieVault/Helper/NetworkHelper.swift
@@ -0,0 +1,30 @@
+//
+//  NetworkHelper.swift
+//  MovieVault
+//
+//  Created by Faza Faresha Affandi on 23/05/25.
+//
+
+import Foundation
+import Network
+
+class NetworkMonitor: ObservableObject {
+    private var monitor: NWPathMonitor
+    private var queue = DispatchQueue(label: "NetworkMonitor")
+
+    @Published var isConnected: Bool = true
+
+    init() {
+        monitor = NWPathMonitor()
+        monitor.pathUpdateHandler = { [weak self] path in
+            DispatchQueue.main.async {
+                self?.isConnected = path.status == .satisfied
+            }
+        }
+        monitor.start(queue: queue)
+    }
+
+    deinit {
+        monitor.cancel()
+    }
+}
